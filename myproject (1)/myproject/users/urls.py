a52e7653e1b260a57395245893adde8b74e918be
kshitizrana123 <122910447+kshitizrana123@users.noreply.github.com>
2024-06-19 21:54:56 +0530

    done

---
+++ myproject (1)/myproject/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('all/', views.ProfileListView.as_view(), name='profile-list-view'),
+    path('follow/', views.follow_unfollow_profile, name='follow-unfollow-view'),
+    path('<int:pk>/', views.ProfileDetailView.as_view(), name='profile-detail-view'),
+    
+]
